8c01949fbe7bb1e4bba1e371f77a8ed095a3a349
Timothy Graupmann <tim.graupmann@razer.com>
2020-01-15 13:46:28 -0800

    Initial commit of project

--- CSDK_ChromaModSample.cpp
+++ CSDK_ChromaModSample.cpp
@@ -2,20 +2,127 @@
 //
 
 #include "pch.h"
+#include <conio.h>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Selections
+{
+public:
+	void DetectMods()
+	{
+		_mSkins.push_back("Base");
+		_mSkins.push_back("Skin1");
+		_mSkins.push_back("Skin2");
+		_mSkins.push_back("Mod1");
+		_mSkins.push_back("Mod2");
+	}
+	vector<string> GetSkins()
+	{
+		return _mSkins;
+	}
+	vector<string> GetClasses()
+	{
+		vector<string> list;
+		list.push_back("Fighter");
+		list.push_back("Paladin");
+		list.push_back("Thief");
+		return list;
+	}
+	vector<string> GetEffects()
+	{
+		vector<string> list;
+		list.push_back("Attack");
+		list.push_back("Block");
+		list.push_back("Damage");
+		return list;
+	}
+	void HandleInput(int input)
+	{
+		switch (input)
+		{
+		case 'Q':
+		case 'q':
+			exit(0);
+			return;
+		case 'S':
+		case 's':
+			_mSkinId = (_mSkinId + 1) % GetSkins().size();
+			break;
+		case 'C':
+		case 'c':
+			_mClassId = (_mClassId + 1) % GetClasses().size();
+			break;
+		case 'E':
+		case 'e':
+			_mEffectId = (_mEffectId + 1) % GetEffects().size();
+			break;
+		}
+	}
+	void DisplayList(vector<string>& list, int index)
+	{
+		int i = 0;
+		for (vector<string>::iterator iter = list.begin(); iter < list.end(); ++iter)
+		{
+			cout << "[";
+			if (i == index)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+			cout << "] " << iter->c_str() << endl;
+			++i;
+		}
+		cout << endl;
+	}
+	void Print()
+	{
+		system("CLS");
+		cout << "Welcome to the C++ Chroma Mod Sample" << endl;
+		cout << "Press [Q] to Quit" << endl << endl;
+
+		vector<string> list;
+
+		cout << "[S] Selected Skin:" << endl;
+		list = GetSkins();
+		DisplayList(list, _mSkinId);		
+
+		cout << "[C] Classes:" << endl;
+		list = GetClasses();
+		DisplayList(list, _mClassId);
+
+		cout << "[E] Chroma Effects:" << endl;
+		list = GetEffects();
+		DisplayList(list, _mEffectId);
+	}
+private:
+	vector<string> _mSkins;
+	int _mSkinId = 0;
+	int _mClassId = 0;
+	int _mEffectId = 0;
+	bool _mShouldQuit = false;
+};
+
+void GameLoop()
+{
+	Selections selections;
+	selections.DetectMods();
+	selections.Print();
+	while (true)
+	{
+		int input = _getch();
+		selections.HandleInput(input);
+		selections.Print();
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	GameLoop();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
